dcd616ee07a84a4d70d5207aa17d798bcff20923
Noah Laferty <noah.lafferty@gmail.com>
2015-10-14 12:45:00 -0500

    restructure for merge

--- temp/assets/js/main.js
+++ temp/assets/js/main.js
@@ -1,9 +1,21 @@
-var $portfolio = $('#portfolio')
+var $portfolio = $('#portfolio');
 
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
+// fake out the browser to check if you redirected from noahlafferty.me/portfolio
+if (document.referrer.endsWith("/portfolio/")) {
+	$portfolio.removeClass('closed');
+	window.history.pushState("portfolioOpen", "Noah Lafferty - Portfolio", "/portfolio/");
+	document.title = "Noah Lafferty - Portfolio";
+}
+
+// toggle the portfolio open or closed on mouse click 
 $('body').on('click', '#portfolio:not(.closed) .portfolio-toggle, #portfolio.closed', function() {
 	if ($portfolio.hasClass('closed')) {
 		$portfolio.removeClass('closed');
-		window.history.pushState("portfolioOpen", "Noah Lafferty - Portfolio", "/");
+		window.history.pushState("portfolioOpen", "Noah Lafferty - Portfolio", "/portfolio");
 		document.title = "Noah Lafferty - Portfolio";
 	} else {
 		$portfolio.addClass('closed');
@@ -12,6 +24,7 @@ $('body').on('click', '#portfolio:not(.closed) .portfolio-toggle, #portfolio.clo
 	}
 });
 
+// do the fancy hover animation when you mouse over a link in my text
 $('.portfolio-link').mouseenter(function() {
 	$portfolio.addClass('hover');
 }).mouseleave(function() {
@@ -20,6 +33,7 @@ $('.portfolio-link').mouseenter(function() {
 	$('.portfolio-toggle').click();
 });
 
+// capture the browser's back button so you can open and close the portfolio
 window.onpopstate = function(e){
     if (e.state === "portfolioOpen") {
     	$portfolio.removeClass('closed');
@@ -29,3 +43,4 @@ window.onpopstate = function(e){
     	document.title = "Noah Lafferty";
     }
 };
+
